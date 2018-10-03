21425cd8e9a67c2f3e1316cbabf726e9c79f3551
jcuadros <jordi.cuadros@iqs.edu>
2018-10-03 23:37:48 +0200

    Added previous versions of IntroR and MinTextos scripts

---
+++ IntroR/IntroR_fet.R
@@ -0,0 +1,399 @@
+##=========================================================================##
+##                                                                         ##
+##  Seminari d'introducció a R                                             ##
+##  @authors: Jordi Cuadros, Francesc Martori, Vanessa Serrano             ##
+##                                                                         ##
+##=========================================================================##
+
+#### ACCÉS A L'AJUDA #### 
+# Cercar ajuda sobre una funció
+? "vector"
+# Cercar un text als documents de l'ajuda
+?? "data types"
+# A RStudio, es pot seleccionar i prémer F1
+
+
+#### DADES BÀSIQUES #### 
+## Numeric
+a <- 2
+a
+class(a)
+b <- 13.6788956789
+b
+class(b)
+print(b, digits = 10)
+
+a+b
+a-b
+a*b
+a^b
+a/b
+
+## Integer
+n <- as.integer(1)
+n <- as.integer(340000)
+class(n)
+
+# En operar un integer es converteix en numeric
+class(n + 1)
+
+## Character
+a <- "aaa"
+b <- "bbb"
+paste(a, b, "hola", sep = ", ")
+
+## Logical 
+a <- 3 == 2 # Comparació igualtat
+a
+b <- 3 != 2 # Comparació diferent
+b
+
+a & b # Operador AND
+a | b # Operador OR
+!b  # Operador NOT
+
+## Complex (menys útil en l'anàlisi de dades)
+a <- 2 + 3i
+class(a)
+a ^ 2
+
+
+#### DADES COMPOSTES #### 
+## Vector
+a <- c(2, 3, 4)
+b <- c(10, 20)
+class(a)
+str(a)
+a[2]
+
+# Totes les dades han de ser del mateix tipus
+c(1,"a","b")
+c(1, 5, T)
+c(T,"a",F)
+c(2, "c", F)
+
+# Les operacions s'apliquen a vectors i retornen vectors
+a + a 
+b * b
+a > 2.5
+
+a * b # Reciclatge de dades
+
+# Funcions amb vectors
+abs(sin(a))
+exp(a)
+sort(-a)
+sort(a,decreasing = T)
+a[order(-a)]
+
+length(a)
+sum(a)
+mean(a)
+sd(a)
+max(a)
+
+3 %in% a
+c(15,10,20,40) %in% b
+
+# Vectors seqüència
+1:10
+seq(1, 10, by = 2)
+seq(1, 3, length.out = 5) 
+
+## List
+a <- list(2, "2", FALSE)
+b <- list(3, "hola", c(2, 3, 4))
+a
+b
+length(a)
+a[[3]]
+b[[3]]
+b[[3]][1]
+str(b)
+unlist(b)
+str(b)
+
+## Factor
+a <- c("hola", "adeu","hola", "adeu", "adeu", "bye")
+a
+b <- as.factor(a)
+b
+as.character(b)
+as.numeric(b)
+str(b)
+
+a <- factor(c(3, 1, 3, 1, 1, 2), labels = c("adeu", "bye", "hola"))
+a
+levels(a)
+
+## Factor ordenat
+notes <- c("Aprovat", "Insuficient", "Notable", "Insuficient", "Notable", "Excel·lent", "Aprovat")
+str(notes)
+notes <- factor(notes)
+str(notes)
+levels(notes)
+notes <- as.character(notes)
+str(notes)
+notes <- factor(notes,
+                levels = c("Insuficient", "Aprovat", "Notable", "Excel·lent"),
+                ordered = TRUE)
+str(notes)
+levels(notes)
+
+## Matrius
+a <- matrix(c(2, 4, -3, 5), ncol = 2)
+a[2,2]
+a * a # Producte posició a posició
+a %*% a # Producte matricial
+
+t(a) # Transposició
+
+## Data Frame
+dfA <- data.frame(int = 1:10, let = sample(letters, 10, replace = TRUE), 
+                  ran = rnorm(10))
+dfA
+dim(dfA) # Dimensio, com si fos una matriu
+nrow(dfA) # Número de columnes
+ncol(dfA) # Número de fileres
+str(dfA)
+head(dfA) # Primers valors, 6 per defecte
+tail(dfA, 2) # Darrers valors
+
+
+#### CONVERSIÓ ENTRE DADES COMPOSTES #### 
+# list <=> vector
+list1 <- list(1:5, letters[20:10], rep("c", 5), c(1, 2, 2, 2))
+list1
+vec1 <- unlist(list1)
+vec1
+list2 <- list(vec1)
+list2
+list2 <- as.list(vec1)
+list2
+
+# matrix <=> data.frame
+df1 <- data.frame(1:5, letters[20:16], rep("c", 5), c(1, 2, 2, 4, 5))
+df1
+mat1 <- as.matrix(df1)
+mat1
+df2 <- data.frame(mat1) # O as.data.frame
+str(df2)
+
+
+#### MANIPULACIÓ DE TAULES DE DADES ####
+# Anomenar files i columnes
+dfTest1 <- data.frame(1:5, letters[1:5], c(rep("a", 3), rep("b", 2)))
+dfTest1
+
+colnames(dfTest1) <- c("var1", "var2", "var3") 
+rownames(dfTest1) <- paste("subject00", 1:5, sep = "")
+dfTest1
+
+# Afegir files i columnes
+dfTest2 <- cbind(dfTest1, rnorm(5)) # afegint un vector al data frame
+dfTest2
+dfTest2$var5 <- 5:1 # assignant els valor a una nova variable
+dfTest2
+
+dfTest2 <- rbind(dfTest1, list(6, "e", "b"))
+dfTest2
+
+# Segmentar (subsetting)
+# ... per índexs
+dfTest2 <- dfTest1[1:3,]
+dfTest2
+
+dfTest2 <- dfTest1[,c(1,3)]
+dfTest2
+
+dfTest2 <- dfTest1[-3,-2]
+dfTest2
+
+# ... per noms
+dfTest2 <- dfTest1[,"var2"]
+dfTest2
+dfTest1$var2
+
+dfTest2 <- dfTest1[,c("var2","var3")]
+dfTest2
+
+# ... per condicions o valors lògics
+dfTest2 <- dfTest1[c(T,T,F,T,F), c(T,F,T)]
+dfTest2
+
+dfTest2 <- dfTest1[dfTest1[,1] == 3 | dfTest1[,3] == "b",]
+dfTest2
+
+# ... per presència en vector
+dfTest2 <- dfTest1[rownames(dfTest1) %in% c("subject001","subject003"),]
+dfTest2
+
+dfTest2 <- dfTest1[,!(colnames(dfTest1) %in% c("var3"))]
+dfTest2
+
+sel<-which(rownames(dfTest1) %in% c("subject001","subject003"))
+sel
+dfTest1[sel,]
+
+# També poden ser interessant which.max i which.min
+set.seed(123)
+a <- runif(10)
+a
+which.max(a)
+a[which.min(a)]
+
+# Eliminar columna
+dfTest2 <- dfTest1
+dfTest2[,2] <- NULL
+dfTest1
+dfTest2
+
+
+#### ELIMINAR UNA VARIABLE #### 
+dfTest2 <- NULL
+rm(dfTest2)
+
+
+#### CONSTANTS #### 
+NA
+NULL
+is.null(NULL)
+is.na(NA)
+is.null(NA)
+is.na(NULL)
+
+Inf
+NaN
+
+exp(999999)
+log(-5)
+log(-5+0i)
+
+#### IMPORTACIÓ I EXPORTACIÓ D'ARXIUS DE DADES #### 
+# https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data
+# https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.names
+
+# mpg:           continuous
+# cylinders:     multi-valued discrete
+# displacement:  continuous
+# horsepower:    continuous
+# weight:        continuous
+# acceleration:  continuous
+# model year:    multi-valued discrete
+# origin:        multi-valued discrete 1-North America, 2-Europe and 3-Asia
+# car name:      string (unique for each instance)
+
+getwd()
+#setwd(...)
+
+carsUCI <- read.table("auto-mpg-fixed.data", header = FALSE, 
+                      sep = "\t", strip.white = TRUE, na.strings = "?", quote = "\"")
+colnames(carsUCI) <- c("mpg","cylinders","displacement","horsepower","weight",
+                       "acceleration","year","origin","carName")
+
+str(carsUCI)
+head(carsUCI)
+
+# Importació d'un conjunt de dades mal formatades o d'amplada fixa
+lines <- readLines("auto-mpg.data")
+str(lines)
+head(lines)
+mpg <- as.numeric(substr(lines,1,4))
+str(mpg)
+
+# Exportació
+# ... a text
+write.table(x = carsUCI, file = "carsUCI.tsv", sep = "\t", dec = ",",row.names = FALSE)
+# ... a RData
+save(x = carsUCI, file = "carsUCI.RData")
+
+# Espai de treball a RData
+save(list = ls(all.names = TRUE), file = "workspace.RData") 
+save.image("workspace.RData")
+
+# Lectura de RData
+load("carsUCI.RData")
+
+print(load("carsUCI.RData"))
+rm(list=ls()) # Esborra el workspace
+loadedDS <- get(load("carsUCI.RData"))
+
+#### ESTADÍSTICA DESCRIPTIVA
+airquality
+?airquality
+
+## Estadístics habituals
+summary(airquality)
+
+mean(airquality) # No OK
+mean(airquality$Ozone) # Compte amb NA's
+mean(airquality$Ozone, na.rm = TRUE)
+median(airquality$Ozone, na.rm = TRUE)
+
+# Càlcul de quantils
+quantile(airquality$Ozone, na.rm = TRUE, c(0.25, 0.5, 0.75))
+
+# Dispersió
+sd(airquality$Ozone, na.rm = TRUE)
+var(airquality$Ozone, na.rm = TRUE)
+IQR(airquality$Ozone, na.rm = TRUE)
+
+## Taules de freqüències
+table(airquality$Month)
+table(airquality$Ozone) # No OK
+table(airquality$Ozone > mean(airquality$Ozone, na.rm = TRUE)) 
+table(airquality$Ozone > mean(airquality$Ozone, na.rm = TRUE), airquality$Month)
+
+## Regressió Lineal
+mod1 <- lm(Temp ~ Wind, data = airquality)
+summary(mod1)
+
+## Ús de funcionals
+apply(airquality, 2, mean, na.rm = TRUE)
+tapply(airquality$Ozone, airquality$Month, mean, na.rm = TRUE)
+
+## Gràfics habituals
+
+# Per una variable
+hist(airquality$Wind) # Histograma
+boxplot(airquality$Wind) # Diagrama de caixa
+boxplot(airquality$Wind ~ airquality$Month) # o més d'una
+
+# Per a dues variables
+plot(airquality$Wind, airquality$Temp) # Gràfic de dispersió
+abline(mod1, col = "red") # Afegim la recta de regressió
+
+pairs(airquality[,1:4]) # Matriu de gràfics de dispersió
+
+#### EXERCICIS #### 
+# A partir del conjunt de dades "Davis"...
+if (!require("car")) {
+  install.packages("car")
+  library("car")
+}
+ds <- Davis
+help("Davis",package="car")
+
+# 1- Quin tipus de dades tenim?
+
+# 2- Quins camps/columnes conté? A quin tipus de dades correspon cada una de elles?
+
+# 3- Quin és el pes mitjà i pes mitjà autoinformat? I per cada gènere?
+
+# 4- Quin és la mitjana de les diferències entre pes i pes autoinformat?
+
+# 5- Feu un diagrama de caixa per al pes d'homes i de dones
+
+# 6- Quantes dones hi ha en el conjunt de dades? I homes?
+
+# 7- Quin és l'individu que presenta un IMC més baix? I el més alt?
+# NOTA: L'IMC es calcula com pes (en kg) entre alçada (en m) al quadrat
+
+# 8- Quin és la mitjana de les diferències entre pes i pes autoinformat per
+# per a les dones? I per als homes?
+
+# 9- Quin és la mitjana de les diferències entre pes i pes autoinformat
+# per a les dones que pesen més que la mediana? I per a les que pesen menys?
+
+# 10- Feu un diagrama de dispersió de l'alçada i del pes, de manera que les dades 
+# d'homes i de dones surtin de colors diferents. Afegiu la recta de regressió.
---
+++ MinTextos/MinTextos.R
@@ -0,0 +1,509 @@
+##=========================================================================##
+##                                                                         ##
+##  Introdució a la mineria de textos amb R                                ##
+##  @authors: Jordi Cuadros, Francesc Martori, Vanessa Serrano             ##
+##                                                                         ##
+##=========================================================================##
+
+#### VARIABLES I FUNCIONS BÀSIQUES DE TEXT ####
+# El text s'emmagatzemma en variables tipus caracter
+text1 <- "Benvinguts al seminari d'introducció a la mineria de textos amb R"
+class(text1)
+
+# Si la informació textual està codificada com un factor cal convertir-la a
+# caracters, usant la funció as.character()
+
+## Determinar el nombre de caràcters ####
+nchar(text1)
+
+## Unir textos ####
+vec1 <- c("Espero", "que", "us", "resulti", "molt", "profitós")
+paste(paste(vec1, collapse=" "),"!", sep="")
+
+## Fragmentar un text ####
+strsplit(text1, " ")
+paraules <- unlist(strsplit(text1, "[ ']"))
+nchar(paraules)
+longs <- nchar(paraules)
+paraules[which.max(longs)]
+lletres <- unlist(strsplit(paraules, ""))
+tabLletres <- table(lletres)
+tabLletres <- tabLletres[order(-tabLletres, names(tabLletres))]
+tabLletres
+
+## Extraure una subcadena ####
+substr(text1, 1, 1)
+substr(text1, 2, 2)
+# Fixeu-vos que el tercer argument és posició final
+
+substr(paraules, 2, 2)
+
+## Canviar la mida de caixa ####
+tolower(paraules)
+toupper(paraules)
+
+## Comparar cadenes ####
+"a" == "A"
+"a" == "a"
+"a" == "a "
+c("a") == c("a", "A")
+letters[order(letters, decreasing = T)]
+letters2 <- c(letters, toupper(letters))
+letters2[order(letters2, decreasing = T)]  # order uses lexicographical order
+
+## Entendre la codificació de caràcters ####
+Sys.getlocale()
+l10n_info()
+
+strtoi(charToRaw("a"),16)
+rawToChar(as.raw(97))
+
+Encoding(text1)
+enc2utf8(text1)
+text1utf <- enc2utf8(text1)
+Encoding(text1utf)
+
+unlist(strsplit(text1,""))
+c(sapply(text1,charToRaw))
+c(sapply(text1utf,charToRaw))
+
+nchar(text1)
+nchar(text1utf)
+nchar(text1, type = "bytes")
+nchar(text1utf, type = "bytes")
+
+rawToChar(as.raw(0xf3))
+rawToChar(as.raw(c(0xb3,0xc3)))
+
+text1mal <- text1utf
+Encoding(text1mal) <- "latin1"
+text1mal
+
+iconv(text1mal, from="UTF-8", to="latin1")
+
+# Caràcters especials en les cadenes de text en R
+# Salt de línia: \r\n (Windows), \n (Linux, UNIX)
+text3 <- "hola\tadéu\r\n\xfe\t\"\t\\"
+cat(text3)
+Encoding(text3)
+
+c(sapply(text3,charToRaw))
+
+
+#### INTRODUCCIÓ A LES EXPRESSIONS REGULARS ####
+## Cerca i substitució a R ####
+text1 <- "Benvinguts al seminari d'introducció a la mineria de textos amb R"
+paraules <- unlist(strsplit(text1,split="[ ,.]"))
+paraules
+
+gsub("a", "_", text1, fixed=TRUE)
+gsub("a", "_", paraules, fixed=TRUE)
+
+grepl("amb", paraules, fixed=TRUE)    # grep és semblant
+paraules == "amb"
+
+regexec("amb", text1, fixed=TRUE)     # regexpr i gregexpr són semblants
+
+trobat <- regexec("amb", text1, fixed=TRUE)
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+trobat <- regexec("t", paraules, fixed=TRUE)
+trobat
+trobat <- sapply(trobat, getElement, 1) != -1
+
+## Expressions regulars ####
+# Les expressions regulars permeten anar més enllà de les cerques de
+# seqüències exactes i cercar a partir de patrons.
+
+# - Expressions alternatives
+regex <- "a|e"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+regex <- "[A-Z]"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+# - Diferent de
+regex <- "[^ae]"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+# - Comodins
+regex <- "i.e"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+# - Multiplicadors
+regex <- "i.*e"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+regex <- "i.*?e"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+regex <- "e..+i"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+regex <- "i.?a"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+regex <- "e.{2,3}i"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+# - Ancoratges a text
+regex <- "^.e"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+regex <- "i.*$"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+# - Ancoratges a paraula
+regex <- "\\<.e"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+regex <- "i\\S*\\>"
+gsub(regex, "_", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+# - Grups i captures
+regex <- "(i[aàeéèoóò])"
+gsub(regex, "[\\1]", text1)
+paraules[grepl(regex,paraules)]
+trobat <- regexec(regex, text1)
+trobat
+substr(text1, trobat[[1]], trobat[[1]] + attr(trobat[[1]], "match.length") - 1)
+
+
+#### TÈCNIQUES BASADES EN LA BOSSA DE PARAULES ####
+if (!require("tm")) {
+  install.packages("tm")
+  library("tm")
+}
+if (!require("wordcloud")) {
+  install.packages("wordcloud")
+  library("wordcloud")
+}
+
+## Crear la bossa de paraules ####
+tirant <- readLines("pg_TheWhiteKnight.txt")
+str(tirant)
+
+Encoding(tirant) <- "UTF-8"
+tirant <- tirant[413:18286]
+
+tirant <- paste(tirant, collapse=" ")
+
+corpus_tirant <- VCorpus(VectorSource(tirant))
+tdm_tirant <- TermDocumentMatrix(corpus_tirant)
+
+frequent <- findMostFreqTerms(tdm_tirant, n = 100)
+frequent
+
+## Representar un núvol de paraules ####
+wordcloud(names(frequent$`1`),frequent$`1`)
+
+## Precondicionar els documents ####
+tirant2 <- tirant
+tirant2 <- tolower(tirant2)
+tirant2 <- gsub("[^[:print:]]"," ", tirant2)
+tirant2 <- gsub("[[:punct:]]"," ", tirant2)
+tirant2 <- gsub("\\<\\w{0,3}\\>"," ", tirant2)
+tirant2 <- gsub("\\s{2,}"," ", tirant2)
+
+tirant2 <- removeWords(tirant2, gsub("[[:punct:]]"," ", stopwords("SMART")))
+
+corpus_tirant <- VCorpus(VectorSource(tirant2))
+tdm_tirant <- TermDocumentMatrix(corpus_tirant)
+frequent <- findMostFreqTerms(tdm_tirant, n = 100)
+wordcloud(names(frequent$`1`),frequent$`1`,rot.per=0,
+          colors=brewer.pal(4,"Spectral"),random.order = FALSE)
+
+## Usar multigrames ####
+if (!require("RWeka")) {
+  install.packages("RWeka")
+  library("RWeka")
+}
+NumbergramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+NumbergramTokenizer("hola, bon dia tingueu.")
+
+corpus_tirant <- VCorpus(VectorSource(tirant2))
+tdm_tirant <- TermDocumentMatrix(corpus_tirant, control=list(tokenize=NumbergramTokenizer))
+frequent <- findMostFreqTerms(tdm_tirant, n = 100)
+wordcloud(names(frequent$`1`),frequent$`1`,rot.per=0,
+          colors=brewer.pal(4,"Spectral"),
+          scale=c(2,.5),random.order = FALSE)
+
+## Treballar amb diversos documents ####
+tirant3 <- unlist(strsplit(tirant2, "chapter"))
+tirant3 <- tirant3[2:length(tirant3)]
+
+corpus_tirant <- VCorpus(VectorSource(tirant3))
+tdm_tirant <- TermDocumentMatrix(corpus_tirant)
+frequent <- findMostFreqTerms(tdm_tirant, n = 100)
+wordcloud(names(frequent$`1`),frequent$`1`,rot.per=0,
+          colors=brewer.pal(4,"Spectral"),random.order = FALSE)
+
+corpus_tirant <- VCorpus(VectorSource(tirant3))
+tdm_tirant <- TermDocumentMatrix(corpus_tirant, 
+                                 control= list(weighting = weightTfIdf))
+frequent <- findMostFreqTerms(tdm_tirant, n = 100)
+wordcloud(names(frequent$`1`),frequent$`1`,rot.per=0,
+          colors=brewer.pal(4,"Spectral"),random.order = FALSE)
+
+commonality.cloud(as.matrix(tdm_tirant[,c(1,15)]), rot.per=0, max.word=100,
+                  colors=brewer.pal(4,"Spectral"),random.order = FALSE)
+comparison.cloud(as.matrix(tdm_tirant[,c(1,15)]), rot.per=0, max.word=200,
+                 scale = c(2,.5), title.size = 2, 
+                 colors=brewer.pal(4,"Spectral"),random.order = FALSE)
+
+## Determinar relacions entre termes ####
+corpus_tirant <- VCorpus(VectorSource(tirant2))
+tdm_tirant <- TermDocumentMatrix(corpus_tirant, control=list(tokenize=NumbergramTokenizer))
+frequent <- findMostFreqTerms(tdm_tirant, n = 100)
+
+if (!require("igraph")) {
+  install.packages("igraph")
+  library("igraph")
+}
+if (!require("ggraph")) {
+  install.packages("ggraph")
+  library("ggraph")
+}
+
+l <- strsplit(names(frequent$`1`),split=" ")
+df <- data.frame(matrix(unlist(l), ncol=2, byrow=T),
+                 stringsAsFactors=FALSE)
+colnames(df) <- c("from", "to")
+relations <- data.frame(df,count=frequent$`1`)
+igraph <- graph_from_data_frame(relations)
+
+ggraph(igraph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = count, edge_width = count),
+                 edge_colour = "royalblue") +
+  geom_node_point(size = 5) +
+  geom_node_text(aes(label = name), repel = TRUE,
+                 point.padding = unit(0.2, "lines")) +
+  theme_void() + theme(legend.position="none")
+
+## Modelar temes (Topic modelling) ####
+if (!require("topicmodels")) {
+  install.packages("topicmodels")
+  library("topicmodels")
+}
+if (!require("tidyverse")) {
+  install.packages("tidyverse")
+  library("tidyverse")
+}
+
+data(AssociatedPress)
+? "AssociatedPress"
+str(AssociatedPress)
+
+ap_lda <- LDA(AssociatedPress, k = 4)
+str(ap_lda)
+
+betas <- as.data.frame(t(ap_lda@beta))
+colnames(betas) <- 1:ncol(betas)
+betas$term <- ap_lda@terms
+
+ap_topics <- betas %>% gather("topic", "beta", -term, factor_key=TRUE)
+
+ap_top_terms <- ap_topics %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta) %>%
+  mutate(term = paste(strrep(" ",as.numeric(topic) - 1),term,sep="")) %>%
+  mutate(beta = 10^beta)
+
+ap_top_terms %>%
+  mutate(term = reorder(term, beta)) %>%
+  ggplot(aes(term, beta, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free_y") +
+  coord_flip()
+
+# Hi ha altres tècniques que permeten agrupar bosses de paraules, 
+# per exemple skmeans.
+
+
+#### EXERCICIS 1 #### 
+# Treballarem amb el text de l'Auca del Senyor Esteve...
+# Està en el fitxer "pg_AucaDelSenyorEsteve.txt"
+
+# 1- Carrega el fitxer de text i ajusta'l de forma que només hi hagi
+# el contingut de l'obra en una variable de text.
+
+# 2- Quantes paraules hi ha a l'obra?
+
+# 3- Quantes paraules diferents hi ha?
+
+# 4- Quina és la paraula més freqüent?
+
+# 5- Quina és la paraula més llarga que apareix a l'obra?
+
+# 6- Crea un núvol de paraules que mostrin les paraules més freqüents de
+#    4 o més lletres
+
+
+#### FUNCIONS DE PROCESSAMENT DE LLENGUATGE NATURAL ####
+## Identificació d'idioma ####
+barc <- readLines("barcelona.txt", encoding = "UTF-8")
+
+if (!require("textcat")) {
+  install.packages("textcat")
+  library("textcat")
+}
+textcat(barc)
+
+if (!require("cld2")) {
+  install.packages("cld2")
+  library("cld2")
+}
+detect_language(barc)
+lapply(barc,detect_language_mixed)
+
+## Identificació de classes gramaticals ####
+if (!require("openNLP")) {
+  install.packages("openNLP")
+  library("openNLP")
+}
+frase1 <- barc[1]
+frase1
+frase1 <- gsub("[(].*?[)]","",frase1)
+frase1 <- gsub("[[].*?[]]","",frase1)
+frase1 <- gsub("\\s{2,}"," ",frase1)
+frase1
+
+frase1 <- as.String(frase1)
+anSent <- NLP::annotate(frase1,Maxent_Sent_Token_Annotator())
+anSent
+frase1[anSent]
+
+anWord <- NLP::annotate(frase1,Maxent_Word_Token_Annotator(),anSent)
+anWord
+frase1[anWord]
+
+anPOS <- NLP::annotate(frase1,Maxent_POS_Tag_Annotator(),anWord)
+anPOSword <- anPOS[anPOS$type=="word"]
+
+tags <- sapply(anPOSword$features, `[[`, "POS")
+tags
+table(tags)
+paste(frase1[anPOSword],"/",tags,sep="")
+frase1[anPOSword][tags=="NN"]
+frase1[anPOSword][grepl("NN",tags)]
+
+## Lematització ####
+ex1 <- c("tree", "trial", "tried", "try", "trees", "be", "is", "are",
+         "run", "ran", "running")
+
+if (!require("tm")) {
+  install.packages("tm")
+  library("tm")
+}
+stemDocument(ex1)
+stemCompletion(stemDocument(ex1),dictionary = ex1)
+
+if (!require("SnowballC")) {
+  install.packages("SnowballC")
+  library("SnowballC")
+}
+wordStem(ex1)
+
+if (!require("textstem")) {
+  install.packages("textstem")
+  library("textstem")
+}
+stem_words(ex1)
+lemmatize_words(ex1)
+
+# TreeTagger és una altra opció que pot ser d'interès
+# - http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/
+# - https://stackoverflow.com/questions/28214148/how-to-perform-lemmatization-in-r
+
+
+## Distancia d'edició ####
+if (!require("stringdist")) {
+  install.packages("stringdist")
+  library("stringdist")
+}
+
+stringdist("ca","abc")
+ex1[stringdist("treed",ex1)==
+      min(stringdist("treed",ex1))]
+
+
+## Usar diccionaris ####
+if (!require("hunspell")) {
+  install.packages("hunspell")
+  library("hunspell")
+}
+
+words <- c("beer", "wiskey", "wine")
+hunspell_check(words)
+
+# El dictionari català es troba a 
+# https://cgit.freedesktop.org/libreoffice/dictionaries/tree/
+paraules <- c("cervesa", "bi", "caba")
+hunspell_check(paraules, dict=dictionary("ca.dic"))
+hunspell_suggest(paraules, dict=dictionary("ca.dic"))
+
+
+#### REFERÈNCIES ####
+# http://biostat.mc.vanderbilt.edu/wiki/pub/Main/RegularExpressionPrimer/regex-primer.pdf
+# https://www.regular-expressions.info/tutorial.html
+# https://www.rstudio.com/wp-content/uploads/2016/09/RegExCheatsheet.pdf
+# https://cran.r-project.org/web/packages/tm/vignettes/tm.pdf
+# https://cran.r-project.org/web/views/NaturalLanguageProcessing.html
+# https://www.tidytextmining.com/
+# https://cran.r-project.org/web/packages/hunspell/vignettes/intro.html
+
+
